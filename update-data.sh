a6baa9453e65dbf7c108bba4837efb54a6142c71
e0145402 <joseph.harrison@windstream.com>
2021-11-21 13:57:54 -0500

    updating datasets

--- update-data.sh
+++ update-data.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-DATASETS=nt patnt taxdb 18S_fungal_sequences 28S_fungal_sequences ITS_RefSeq_Fungi
+
+WORKDIR=/blast/blastdb
+DATASETS="nt patnt taxdb 18S_fungal_sequences 28S_fungal_sequences ITS_RefSeq_Fungi"
+
+cd $WORKDIR || exit 1
 
 # shellcheck disable=SC2153
 for DATASET in ${DATASETS}
 do
+    echo "updating database: ${DATASET}"
     update_blastdb.pl "${DATASET}" --decompress
 done
