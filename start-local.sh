246a524bfa61b5f18069319c7bd2b4922f8cc762
e0145402 <joseph.harrison@windstream.com>
2021-11-23 11:42:52 -0500

    simplify docker net

--- start-local.sh
+++ start-local.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 [[ -z $PROJECT ]] && PROJECT=mycolab
 export PROJECT
-docker-net.sh
+./docker-net.sh
 
 mkdir -p $HOME/data/${PROJECT}/ncbi-blast/blastdb
 mkdir -p $HOME/data/${PROJECT}/ncbi-blast/fasta
