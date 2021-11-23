22ce5ffd26b6ebb4cba7e094cc635e5b47b407cd
e0145402 <joseph.harrison@windstream.com>
2021-11-23 11:42:20 -0500

    simplify docker net

--- docker-net.sh
+++ docker-net.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-if [ -f venv/bin/activate ]; then 
-  source venv/bin/activate
-fi
 
 if [[ ! $(docker network ls -q -f name="${PROJECT}") ]]; then
   docker network create "${PROJECT}" 2> /dev/null
