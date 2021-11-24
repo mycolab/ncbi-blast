eda1599303e5f5e7f61cc8ac02cf821da3bc9f39
e0145402 <joseph.harrison@windstream.com>
2021-11-24 10:50:56 -0500

    feature(start-local) adding base_path arg

--- start-local.sh
+++ start-local.sh
@@ -4,14 +4,14 @@
 export PROJECT
 
 BASE_PATH=$1
-[[ -z $BASE_PATH ]] && BASE_PATH="${HOME}/data/${PROJECT}"
+[[ -z $BASE_PATH ]] && BASE_PATH="${HOME}/data/${PROJECT}/ncbi-blast"
 
 if [[ ! -d $BASE_PATH ]];then
    echo "${BASE_PATH} is not a valid directory."
    exit 1
 fi
 
-blastdb_path="${BASE_PATH}/ncbi-blast/blastdb"
+blastdb_path="${BASE_PATH}/blastdb"
 if [[ ! -d "${blastdb_path}" ]];then
     echo "creating blastdb directory: $blastdb_path"
     mkdir -p "${blastdb_path}"
@@ -19,12 +19,12 @@ else
     echo "blastdb directory: $blastdb_path"
 fi
 
-fasta_path="${BASE_PATH}/ncbi-blast/fasta"
+fasta_path="${BASE_PATH}/fasta"
 if [[ ! -d "${fasta_path}" ]];then
-    echo "creating fasta directory: $blastdb_path"
+    echo "creating fasta directory: $fasta_path"
     mkdir -p "${fasta_path}"
 else
-    echo "fasta directory: $blastdb_path"
+    echo "fasta directory: $fasta_path"
 fi
 
 ./docker-net.sh || exit 1
